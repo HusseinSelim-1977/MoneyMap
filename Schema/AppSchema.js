b98e32fecf4343cab9cd5e9707c5df0d39340a6b
HusseinSelim-1977 <husseinsherif2006@gmail.com>
2025-10-20 14:35:22 +0300

    Created: AppSchema.js

--- Schema/AppSchema.js
+++ Schema/AppSchema.js
@@ -1,10 +1,38 @@
-const mongoose = require('mongoose');
+import { Schema, model } from 'mongoose';
 
-const AppSchema = new mongoose.Schema({
-    user: {
-    type: mongoose.Schema.Types.ObjectId,
+const AppSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
     required: true,
-    ref: 'User'}
+    ref: 'User'},
+  
+  Bill: [{
+    title: {type: String, required: [true, 'Please Add Billing Title'], unique: true, trim: true},
+    amountNeeded: {type: mongoose.Decimal128, required: [true, 'Please Enter Bill Amount']},
+    amountDeposited:{type: mongoose.Decimal128, required: [true, 'Please Enter Deposited Amount This Month']},
+    DueDate:{type: Date, required: [true, 'Please Enter Bill DueDate']}
+  }],
+
+  Investment: [{
+    title: {type: String, required: [true, 'Please Enter Investment Title'], unique: true, trim: true},
+    amountNeeded: {type: mongoose.Decimal128, required: [true, 'Please Enter Investment Amount']},
+    amountDeposited:{type: mongoose.Decimal128, required: [true, 'Please Enter Deposited Amount This Month']}
+  }],
+
+  Spending: [{
+    title: {type: String, required: [true, 'Please Enter Spending  Title'], unique: true, trim: true},
+    amountDeposited:{type: mongoose.Decimal128, required: [true, 'Please Enter Deposited Amount This Month']}
+  }]  
 }); 
 
-module.exports = mongoose.model('Application', AppSchema);
+AppSchema.pre('save', async function(next) {
+    if (this.isModified('Bill.amountNeeded') || this.isModified('Bill.amountDeposited')) {
+        if (this.Bill.amountNeeded > this.Bill.amountDeposited) {
+            const error = new Error("You're Short This Month 😕");
+            return next(error);
+        }
+    }
+    
+    next();
+});
+export default model('Application', AppSchema);
