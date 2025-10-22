5e7345259bc601cf896255fff584d565ae4ef95b
HusseinSelim-1977 <husseinsherif2006@gmail.com>
2025-10-22 12:55:56 +0300

    Installed: @nest/common package

---
+++ Backend/money-map-controller/money-map-controller.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { MoneyMapControllerController } from './money-map-controller.controller';
+
+describe('MoneyMapControllerController', () => {
+  let controller: MoneyMapControllerController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [MoneyMapControllerController],
+    }).compile();
+
+    controller = module.get<MoneyMapControllerController>(MoneyMapControllerController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
